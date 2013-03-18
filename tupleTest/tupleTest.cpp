fd54c4552fc1777203bff712d4f30e1a8cea2bb2
pekkaRo <roytta@gmail.com>
2013-03-18 15:08:47 +0100

    Now the tuple sort and struct sort work

--- tupleTest/tupleTest.cpp
+++ tupleTest/tupleTest.cpp
@@ -16,11 +16,18 @@ struct co_struct {
   int idx;
 };
 
-bool mycomp(coord_type& a,coord_type& b)
+bool mycomp(coord_type a,coord_type b)
 {
   if(get<0>(a) == get<0>(b))
     return (get<1>(a) == get<1>(b));
   return (get<0>(a) < get<0>(b));
+}
+
+bool struct_comp(co_struct l, co_struct r)
+{
+  if (l.x == r.x)
+    return (l.y < r.y);
+  return (l.x < r.x);
 }
 
 int main()
@@ -31,11 +38,12 @@ int main()
   default_random_engine re;
   default_random_engine::result_type const seedval = time(NULL);
   re.seed(seedval);
-  const int nInput = 3;
+  const int nInput = 5;
   double aa;
   double bb;
   array<tuple<double,double,int>,nInput> coord2;
   array<int,nInput> cpparr;
+  array<co_struct,nInput> coord1;
   aa = unif(re);
   bb = unif(re);
   for(int i=0; i<nInput; i++){
@@ -44,17 +52,40 @@ int main()
     get<0>(coord2[i])=aa;
     get<1>(coord2[i])=bb;
     get<2>(coord2[i])=i;
-    cpparr.at(i)=i;
+    coord1[i].x=aa;
+    coord1[i].y=bb;
+    coord1[i].idx=i;
+    cpparr.at(i)=3-i;
   }
   //cout << get<2>(coord2[0]) << endl;
   cout << cpparr.size() << endl;
   cout << coord2.begin()<<endl;
+  for( int i=0; i< nInput; i++)
+    {
+      cout << "The coordinates in struct are: " << coord1[i].x << " and " 
+       << coord1[i].y << " The index is: " << coord1[i].idx << endl;
+    }
   cout << "The first coordinates are: " << get<0>(coord2[0]) << " and " 
        << get<1>(coord2[0]) << " The index is: " << get<2>(coord2[0]) << endl;
 
-  cout << mycomp(coord2[0],coord2[1])<<endl;
+  cout << struct_comp(coord1[0],coord1[1])<<endl;
   sort(cpparr.begin(),cpparr.end());
+  sort(coord1.begin(),coord1.end(),struct_comp);
   sort(coord2.begin(),coord2.end(),mycomp);
+
+ for( int i=0; i< nInput; i++)
+    {
+      cout << "The coordinates in struct after sort are: " << coord1[i].x << " and " 
+       << coord1[i].y << " The index is: " << coord1[i].idx << endl;
+    }
+
+ cout << "Now Let's see the tuple sort:" << endl;
+
+ for( int i=0; i< nInput; i++)
+    {
+      cout << "The coordinates in struct after sort are: " << get<0>(coord2[i]) << " and " 
+	   << get<1>(coord2[i]) << " The index is: " << get<2>(coord2[i]) << endl;
+    }
 
 
   // cout << "The first coordinates after sort are: " << get<0>(coord2[0]) << " and " 
